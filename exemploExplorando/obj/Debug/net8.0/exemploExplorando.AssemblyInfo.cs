b510ac36a9304bb52858058785915470c72549d2
Amanda <amandacapistrano@outlook.com>
2023-12-15 18:29:43 -0300

    Coleções : Queue - introdução a fila

--- exemploExplorando/obj/Debug/net8.0/exemploExplorando.AssemblyInfo.cs
+++ exemploExplorando/obj/Debug/net8.0/exemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f5e081ab1899ce5b7250e60491f94ebc90b0632")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee1f7d6186d7b63175029923cd050a263d681d11")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
